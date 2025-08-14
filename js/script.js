7a33d8579fd388a03209dc9ea21d3f23e5bfbbc1
chagoo <33360959+chagoo@users.noreply.github.com>
2025-08-13 20:58:16 -0600

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-Resources
-$("#output").pivotUI(
-  $.pivotUtilities.tipsData, {
-    rows: ["sex", "smoker"],
-    cols: ["day", "time"],
-    vals: ["tip", "total_bill"],
-    aggregatorName: "Sum over Sum",
-    rendererName: "Heatmap"
-  });
